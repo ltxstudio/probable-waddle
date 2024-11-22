43e7c97c30ece6f34eee7c722f3f633afa958e2b
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-22 17:58:51 +0600

    Update CodeDisplay.jsx

--- src/components/CodeDisplay.jsx
+++ src/components/CodeDisplay.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import classNames from "classnames";
+import { Light as SyntaxHighlighter } from "react-syntax-highlighter";
+import { docco, dracula } from "react-syntax-highlighter/dist/esm/styles/hljs";
 
-const CodeDisplay = ({ snippet }) => {
+const CodeDisplay = ({ snippet, language, theme }) => {
   return (
-    <Box
-      sx={{
-        backgroundColor: "#f4f4f4",
-        p: 2,
-        borderRadius: 2,
-        mb: 3,
-        overflow: "auto",
-        fontFamily: "monospace",
-        fontSize: 14,
-        maxHeight: 150,
-      }}
+    <div
+      className={classNames("code-display", {
+        "dark-theme": theme === "dark",
+        "light-theme": theme === "light",
+      })}
     >
-      <Typography component="pre">{snippet}</Typography>
-    </Box>
+      <SyntaxHighlighter
+        language={language}
+        style={theme === "dark" ? dracula : docco}
+      >
+        {snippet}
+      </SyntaxHighlighter>
+    </div>
   );
 };
 
