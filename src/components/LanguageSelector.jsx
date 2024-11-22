c803fb3bda4b804bfe8921c31ceb0676c6c06e42
Ltxstudio <ltxstudio.info@gmail.com>
2024-11-22 18:05:00 +0600

    Update LanguageSelector.jsx

--- src/components/LanguageSelector.jsx
+++ src/components/LanguageSelector.jsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { MenuItem, Select, FormControl, InputLabel } from "@mui/material";
+import { MenuItem, Select, FormControl, InputLabel, Box } from "@mui/material";
+import { Code as CodeIcon } from "@mui/icons-material"; // Generic code icon
+import { Python, JavaScript, CPlusPlus, Java, Go } from '@mui/icons-material';
 
 const LanguageSelector = ({ selectedLanguage, onChange }) => {
-  const languages = ["javascript", "python", "c", "java", "go"];
+  const languages = [
+    { label: "JavaScript", value: "javascript", icon: <JavaScript /> },
+    { label: "Python", value: "python", icon: <Python /> },
+    { label: "C", value: "c", icon: <CodeIcon /> },
+    { label: "Java", value: "java", icon: <Java /> },
+    { label: "Go", value: "go", icon: <Go /> },
+  ];
 
   return (
     <FormControl fullWidth sx={{ my: 3 }}>
@@ -11,10 +19,19 @@ const LanguageSelector = ({ selectedLanguage, onChange }) => {
         value={selectedLanguage}
         onChange={(e) => onChange(e.target.value)}
         label="Select Language"
+        sx={{
+          fontSize: { xs: "14px", sm: "16px" }, // Make font size responsive
+          backgroundColor: { xs: "#f5f5f5", sm: "#fff" }, // Background color on mobile
+          borderRadius: 2,
+          '& .MuiSelect-icon': {
+            fontSize: { xs: 22, sm: 24 }, // Make the dropdown icon responsive
+          },
+        }}
       >
-        {languages.map((lang) => (
-          <MenuItem key={lang} value={lang}>
-            {lang.toUpperCase()}
+        {languages.map(({ label, value, icon }) => (
+          <MenuItem key={value} value={value} sx={{ display: "flex", alignItems: "center" }}>
+            <Box sx={{ mr: 2 }}>{icon}</Box> {/* Language Icon */}
+            {label}
           </MenuItem>
         ))}
       </Select>
